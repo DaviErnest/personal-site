3ae9b08015d8778d7b65153332ad70b2f04269b1
Davi Ernest <130423713+DaviErnest@users.noreply.github.com>
2023-06-21 09:50:32 +0900

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Davi Ernest Pradipta',
+  siteName: 'Davi Ernest',
   siteDescription:
     'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
   siteType: 'Person', // schema
