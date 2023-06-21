15703773f4130e78ece6946c323387b38f1ee647
Davi Ernest <130423713+DaviErnest@users.noreply.github.com>
2023-06-21 19:38:59 +0900

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,16 +1,15 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
   siteName: 'Davi Ernest',
-  siteDescription:
-    'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
+  siteDescription: 'Personal site of Davi Ernest Pradipta.',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
-  authorEmail: '', // i.e. hola@lenesaile.com - email of the author
-  authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
+  author: 'Davi Ernest Pradipta', // i.e. Lene Saile - author's name. Must be set.
+  authorEmail: 'general@daviernest.com', // i.e. hola@lenesaile.com - email of the author
+  authorWebsite: 'https://www.daviernest.com', // i.e. https.://www.lenesaile.com - the personal site of the author
+  themeColor: '#323264', //  Manifest: defines the default theme color for the application
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
@@ -22,7 +21,7 @@ module.exports = {
   },
   blog: {
     // this is for the rss feed
-    name: 'My great Web Development Blog',
+    name: 'Davi Ernest Blog',
     description:
       'Tell the word what you are writing about in your blog! It will show up on feed readers.'
   },
@@ -31,14 +30,14 @@ module.exports = {
   },
   address: {
     // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Organization name',
-    street: '123 Main St.',
-    city: 'Ciudad',
-    state: 'Estado',
-    zip: '12345',
-    mobileDisplay: '+34 1234567',
-    mobileCall: ' +341234567',
-    email: 'hola@yoursite.com',
+    firma: 'Davi Ernest Pradipta',
+    street: 'Tokura-cho 14-1',
+    city: 'Hakodate-shi',
+    state: 'Hokkaido',
+    zip: '042-0953',
+    mobileDisplay: '+81 70-1139-8435',
+    mobileCall: ' +817011398435',
+    email: 'general@daviernest.com',
     cif: ''
   },
   menu: {
